e8692ef9e9f56f8a9a1c0316369b1770f8f2f795
mehdiiotmar <148407538+mehdiiotmar@users.noreply.github.com>
2024-11-14 01:36:23 +0000

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
-
+# CSS pour masquer l'icône GitHub
+hide_github_icon = """
+<style>
+.viewerBadge_container__1QSob { display: none !important; }
+</style>
+"""
+st.markdown(hide_github_icon, unsafe_allow_html=True)
 st.title("🎈 My new app")
 st.write(
     "hello mehdi "
